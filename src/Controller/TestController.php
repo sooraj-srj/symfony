4c5f0ef5a8812f7d167e7911a2224e594cc3f762
Ganesh Yarab <ganesh@smart-cover.co.uk>
2018-11-15 16:55:16 +0530

    Initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index()
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+
+    public function my($value='')
+    {
+    	# code...
+    }
+}
